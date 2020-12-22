fe52ad73f5d42923b52a385d15ae67b7afedec39
Yoshinaga-iwnl <yoshinaga.git@gmail.com>
2020-12-22 22:33:10 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <test />
+</template>
+
+<script>
+import Test from '@/components/atoms/Test/Index'
+export default {
+  name: 'app',
+  computed: {
+  },
+  components: {
+    Test
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ons-splitter-side[side=left][animation=overlay] {
+  border-right: 1px solid #BBB;
+}
+</style>
